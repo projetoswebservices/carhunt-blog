137ddd101a31a6b61f11413c9dd0cae33dccaab9
felipersan <feliperodrigues.san@gmail.com>
2023-02-23 22:12:25 -0300

    change blog url

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd1c77de3ee8541a4b3811c1accc926d8',
+  rootNotionPageId: '535c514a47984f1fb76b31f25bbba384',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Agência Sanfilippo',
+  name: 'CarHunt',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Felipe Sanfilippo',
+  author: 'CarHunt',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
@@ -19,8 +19,8 @@ export default siteConfig({
   // social usernames (optional)
   // twitter: 'transitive_bs',
   // github: 'transitive-bullshit',
-  linkedin: 'felipe-sanfilippo-ab035b1a3/',
-  whatsapp: 'https://wa.me/5511989893698',
+  // linkedin: 'felipe-sanfilippo-ab035b1a3/',
+  // whatsapp: 'https://wa.me/5511989893698',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
