f27fb852e8a6937b75a8aed362dc03412dd1a838
felipersan <feliperodrigues.san@gmail.com>
2023-02-23 22:15:02 -0300

    feat - branch dev

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'CarHunt',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Blog CarHunt',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
